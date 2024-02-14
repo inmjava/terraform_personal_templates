fa6f70c2260586d2351f9aca96ac5b1e715d6530
Ivan Nicoli <inmjava@yahoo.com.br>
2024-02-13 23:25:22 -0300

    cloud9 script

--- main.tf
+++ main.tf
@@ -1,8 +1,22 @@
+# Define a região da AWS
 provider "aws" {
-  region = "us-east-1"  # Altere para a região desejada
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # AMI da camada gratuita
+resource "aws_cloud9_environment_ec2" "myide" {
   instance_type = "t2.micro"
+  name          = "myide-env"
+  image_id      = "amazonlinux-2023-x86_64"
+}
+
+data "aws_instance" "cloud9_instance" {
+  filter {
+    name = "tag:aws:cloud9:environment"
+    values = [
+    aws_cloud9_environment_ec2.myide.id]
+  }
+}
+
+output "cloud9_url" {
+  value = "https://us-east-1.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.myide.id}"
 }
